541e1064a1257479a332612f2a2240b12f88a033
kumarjai1 <jaiessarani@gmail.com>
2019-11-05 16:22:27 -0500

    song get mapping list songs

--- songs-api/src/main/java/com/example/songsapi/SongsApiApplication.java
+++ songs-api/src/main/java/com/example/songsapi/SongsApiApplication.java
@@ -1,10 +1,16 @@
 package com.example.songsapi;
 
+import com.example.songsapi.model.Song;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,6 +24,11 @@ public class SongsApiApplication {
 	@RequestMapping("/")
 	public String home() {
 		return "some songs";
+	}
+
+	@GetMapping("/list")
+	public Iterable<Song> listSongs() {
+		return new ArrayList<Song>();
 	}
 
 }
